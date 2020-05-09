1c331f0f7ff2c81a149ab9e40e9a479e42f0f493
ED1FF <alekarp11@gmail.com>
2020-05-09 14:42:34 +0300

    Fix up migration

--- db/1_initial.tx.up.sql
+++ db/1_initial.tx.up.sql
@@ -1,8 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users(
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
 	username VARCHAR (50) UNIQUE NOT NULL,
 	email VARCHAR (355) UNIQUE NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL
 );
-ALTER TABLE users ADD id uuid PRIMARY KEY DEFAULT uuid_generate_v4();
